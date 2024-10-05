be968115768b5f18fecc5606c892e0d9bfa0ea6c
Viktor1504 <viktordenisevich6@gmail.com>
2024-10-05 18:54:56 +0300

    1-todolist finish

--- 1-todolist/src/App.tsx
+++ 1-todolist/src/App.tsx
@@ -1,28 +1,26 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
+import {TaskType, Todolist} from './Todolist'
 
 function App() {
+    const tasks1: TaskType[] = [
+        {id: 1, title: 'HTML&CSS', isDone: true},
+        {id: 2, title: 'JS', isDone: true},
+        {id: 3, title: 'ReactJS', isDone: false},
+        {id: 4, title: 'Redux', isDone: false},
+        {id: 5, title: 'Typescript', isDone: false},
+        {id: 6, title: 'RTK query', isDone: false},
+    ]
+
+    const tasks2: TaskType[] = [
+    ]
+
     return (
         <div className="App">
-            <div>
-                <h3>What to learn</h3>
-                <div>
-                    <input/>
-                    <button>+</button>
-                </div>
-                <ul>
-                    <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
-                    <li><input type="checkbox" checked={true}/> <span>JS</span></li>
-                    <li><input type="checkbox" checked={false}/> <span>React</span></li>
-                </ul>
-                <div>
-                    <button>All</button>
-                    <button>Active</button>
-                    <button>Completed</button>
-                </div>
-            </div>
+            <Todolist title="What to learn" tasks={tasks1}/>
+            <Todolist title="Songs" tasks={tasks2}/>
         </div>
-    );
+    )
 }
 
-export default App;
+export default App
