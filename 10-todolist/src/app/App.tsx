3c40cce7182f90bd074e920c184b5c37239c0cad
Viktor1504 <viktordenisevich6@gmail.com>
2024-10-08 19:39:16 +0300

    10-todolist finish

---
+++ 10-todolist/src/app/App.tsx
@@ -0,0 +1,163 @@
+import './App.css';
+import {Todolist} from '../Todolist';
+import React, {useReducer, useState} from 'react';
+import {v1} from 'uuid';
+import {AddItemForm} from '../AddItemForm';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Unstable_Grid2';
+import Paper from '@mui/material/Paper';
+import {MenuButton} from '../MenuButton';
+import {createTheme, ThemeProvider} from '@mui/material/styles';
+import Switch from '@mui/material/Switch';
+import CssBaseline from '@mui/material/CssBaseline';
+import {
+    addTodolistAC,
+    changeTodolistFilterAC,
+    changeTodolistTitleAC,
+    removeTodolistAC,
+    todolistsReducer
+} from '../model/todolists-reducer';
+import {addTaskAC, changeTaskStatusAC, changeTaskTitleAC, removeTaskAC, tasksReducer} from '../model/tasks-reducer';
+import {useDispatch, useSelector} from 'react-redux';
+import {RootState} from './store';
+
+export type TaskType = {
+    id: string
+    title: string
+    isDone: boolean
+}
+
+export type FilterValuesType = 'all' | 'active' | 'completed'
+
+export type TodolistType = {
+    id: string
+    title: string
+    filter: FilterValuesType
+}
+
+export type TasksStateType = {
+    [key: string]: TaskType[]
+}
+
+type ThemeMode = 'dark' | 'light'
+
+function App() {
+    const todolists = useSelector<RootState, TodolistType[]>(state => state.todolists)
+    const tasks = useSelector<RootState, TasksStateType>(state => state.tasks)
+
+    const dispatch = useDispatch()
+
+    const [themeMode, setThemeMode] = useState<ThemeMode>('light')
+
+    const theme = createTheme({
+        palette: {
+            mode: themeMode === 'light' ? 'light' : 'dark',
+            primary: {
+                main: '#087EA4',
+            },
+        },
+    });
+
+    const removeTask = (taskId: string, todolistId: string) => {
+        dispatch(removeTaskAC({taskId, todolistId}))
+    }
+
+    const addTask = (title: string, todolistId: string) => {
+        dispatch(addTaskAC({todolistId, title}))
+    }
+
+    const changeTaskStatus = (taskId: string, taskStatus: boolean, todolistId: string) => {
+        dispatch(changeTaskStatusAC({taskId, isDone: taskStatus, todolistId}))
+    }
+
+    const changeFilter = (filter: FilterValuesType, id: string) => {
+        dispatch(changeTodolistFilterAC({id, filter}))
+    }
+
+    const removeTodolist = (todolistId: string) => {
+        dispatch(removeTodolistAC(todolistId))
+        dispatch(removeTodolistAC(todolistId))
+    }
+
+    const addTodolist = (title: string) => {
+        dispatch(addTodolistAC(title))
+    }
+
+    const updateTask = (todolistId: string, taskId: string, title: string) => {
+        dispatch(changeTaskTitleAC({taskId, title, todolistId}))
+    }
+
+    const updateTodolist = (id: string, title: string) => {
+        dispatch(changeTodolistTitleAC({id, title}))
+    }
+
+    const changeModeHandler = () => {
+        setThemeMode(themeMode === 'light' ? 'dark' : 'light')
+    }
+
+    return (
+        <ThemeProvider theme={theme}>
+            <CssBaseline/>
+            <AppBar position="static" sx={{mb: '30px'}}>
+                <Toolbar sx={{display: 'flex', justifyContent: 'space-between'}}>
+                    <IconButton color="inherit">
+                        <MenuIcon/>
+                    </IconButton>
+                    <div>
+                        <MenuButton>Login</MenuButton>
+                        <MenuButton>Logout</MenuButton>
+                        <MenuButton background={theme.palette.primary.dark}>Faq</MenuButton>
+                        <Switch color={'default'} onChange={changeModeHandler}/>
+                    </div>
+                </Toolbar>
+            </AppBar>
+            <Container fixed>
+                <Grid container sx={{mb: '30px'}}>
+                    <AddItemForm addItem={addTodolist}/>
+                </Grid>
+
+                <Grid container spacing={4}>
+                    {todolists.map((tl) => {
+
+                        const allTodolistTasks = tasks[tl.id]
+                        let tasksForTodolist = allTodolistTasks
+
+                        if (tl.filter === 'active') {
+                            tasksForTodolist = allTodolistTasks.filter(task => !task.isDone)
+                        }
+
+                        if (tl.filter === 'completed') {
+                            tasksForTodolist = allTodolistTasks.filter(task => task.isDone)
+                        }
+
+                        return (
+                            <Grid key={tl.id}>
+                                <Paper sx={{p: '0 20px 20px 20px'}}>
+                                    <Todolist
+                                        todolistId={tl.id}
+                                        title={tl.title}
+                                        tasks={tasksForTodolist}
+                                        removeTask={removeTask}
+                                        changeFilter={changeFilter}
+                                        addTask={addTask}
+                                        changeTaskStatus={changeTaskStatus}
+                                        filter={tl.filter}
+                                        removeTodolist={removeTodolist}
+                                        updateTask={updateTask}
+                                        updateTodolist={updateTodolist}
+                                    />
+                                </Paper>
+                            </Grid>
+                        )
+                    })}
+                </Grid>
+            </Container>
+        </ThemeProvider>
+    );
+}
+
+export default App;
--- 10-todolist/src/model/todolists-reducer.ts
+++ 10-todolist/src/model/todolists-reducer.ts
@@ -1,54 +1,51 @@
-import {FilterValuesType, TodolistType} from "../App";
-import {v1} from "uuid";
+import {FilterValuesType, TodolistType} from '../app/App';
+import {v1} from 'uuid';
 
+const initialState: TodolistType[] = []
 
-let todolistID1 = v1()
-let todolistID2 = v1()
+export const todolistsReducer = (state: TodolistType[] = initialState, action: ActionsType): TodolistType[] => {
+    switch (action.type) {
+        case 'REMOVE-TODOLIST': {
+            return state.filter(tl => tl.id !== action.payload.id)
+        }
 
-const initialState: TodolistType[] = [
-	{id: todolistID1, title: 'What to learn', filter: 'all'},
-	{id: todolistID2, title: 'What to buy', filter: 'all'},
-]
+        case 'ADD-TODOLIST': {
+            const newTodolist: TodolistType = {
+                id: action.payload.todolistId,
+                title: action.payload.title,
+                filter: 'all'
+            }
+            return [...state, newTodolist]
+        }
 
-export const todolistsReducer = (state: TodolistType[] = initialState, action: ActionsType) => {
-	switch (action.type) {
-		case 'REMOVE-TODOLIST': {
-			return state.filter(tl => tl.id !== action.payload.id)
-		}
+        case 'CHANGE-TODOLIST-TITLE': {
+            return state.map(tl => tl.id === action.payload.id ? {...tl, title: action.payload.title} : tl)
+        }
 
-		case 'ADD-TODOLIST': {
-			const newTodolist: TodolistType = {id: action.payload.todolistId, title: action.payload.title, filter: 'all'}
-			return [...state, newTodolist]
-		}
+        case 'CHANGE-TODOLIST-FILTER': {
+            return state.map(tl => tl.id === action.payload.id ? {...tl, filter: action.payload.filter} : tl)
+        }
 
-		case 'CHANGE-TODOLIST-TITLE': {
-			return state.map(tl => tl.id === action.payload.id ? {...tl, title: action.payload.title} : tl)
-		}
-
-		case 'CHANGE-TODOLIST-FILTER': {
-			return state.map(tl => tl.id === action.payload.id ? {...tl, filter: action.payload.filter} : tl)
-		}
-
-		default:
-			throw new Error("I don't understand this type")
-	}
+        default:
+            return state
+    }
 }
 
 // Action creators
-export const removeTodolistAC = (todolistId: string) => {
-	return {type: 'REMOVE-TODOLIST', payload: {id: todolistId}} as const
+export const removeTodolistAC = (id: string) => {
+    return {type: 'REMOVE-TODOLIST', payload: {id}} as const
 }
 
 export const addTodolistAC = (title: string) => {
-	return {type: 'ADD-TODOLIST', payload: {title, todolistId: v1()}} as const
+    return {type: 'ADD-TODOLIST', payload: {title, todolistId: v1()}} as const
 };
 
-export const changeTodolistTitleAC = (id: string, title: string) => {
-	return {type: 'CHANGE-TODOLIST-TITLE', payload: {id, title}} as const
+export const changeTodolistTitleAC = (payload: { id: string, title: string }) => {
+    return {type: 'CHANGE-TODOLIST-TITLE', payload} as const
 };
 
-export const changeTodolistFilterAC = (id: string, filter: FilterValuesType) => {
-	return {type: 'CHANGE-TODOLIST-FILTER', payload: {id, filter}} as const
+export const changeTodolistFilterAC = (payload: { id: string, filter: FilterValuesType }) => {
+    return {type: 'CHANGE-TODOLIST-FILTER', payload} as const
 }
 
 // Actions types
@@ -58,7 +55,7 @@ export type ChangeTodolistTitleActionType = ReturnType<typeof changeTodolistTitl
 export type ChangeTodolistFilterActionType = ReturnType<typeof changeTodolistFilterAC>
 
 type ActionsType = RemoveTodolistActionType
-	| AddTodolistActionType
-	| ChangeTodolistTitleActionType
-	| ChangeTodolistFilterActionType
+    | AddTodolistActionType
+    | ChangeTodolistTitleActionType
+    | ChangeTodolistFilterActionType
 
