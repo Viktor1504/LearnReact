ca5e17d17390c6029eec1bd37f464a059c054028
Viktor1504 <viktordenisevich6@gmail.com>
2024-11-06 01:55:36 +0300

    18-todolist_new-finish-update_filter

--- Todolist/18-todolist_new/src/features/todolists/ui/Todolists/Todolist/Tasks/Tasks.tsx
+++ Todolist/18-todolist_new/src/features/todolists/ui/Todolists/Todolist/Tasks/Tasks.tsx
@@ -1,42 +1,41 @@
 import List from "@mui/material/List"
-import { useEffect } from "react"
 import { TaskStatus } from "common/enums"
-import { useAppDispatch } from "common/hooks"
-import { fetchTasksTC } from "../../../../model/tasksSlice"
-import { DomainTodolist } from "../../../../model/todolistsSlice"
+import { DomainTodolist, FilterValuesType } from "../../../../model/todolistsSlice"
 import { Task } from "./Task/Task"
 import { useGetTasksQuery } from "../../../../api/_tasksApi"
+import { DomainTask } from "../../../../api/tasksApi.types"
 
 type Props = {
   todolist: DomainTodolist
 }
 
 export const Tasks = ({ todolist }: Props) => {
-  const { data: tasks } = useGetTasksQuery(todolist.id)
+  const { data } = useGetTasksQuery(todolist.id)
+  const tasks = data?.items || []
 
-  const dispatch = useAppDispatch()
-
-  useEffect(() => {
-    dispatch(fetchTasksTC(todolist.id))
-  }, [])
-
-  let tasksForTodolist = tasks?.items
-
-  if (todolist.filter === "active") {
-    tasksForTodolist = tasksForTodolist?.filter((task) => task.status === TaskStatus.New)
+  const filterTasks = (tasks: DomainTask[], filter: FilterValuesType): DomainTask[] => {
+    switch (filter) {
+      case "active": {
+        return tasks.filter((task) => task.status === TaskStatus.New)
+      }
+      case "completed": {
+        return tasks.filter((task) => task.status === TaskStatus.Completed)
+      }
+      default: {
+        return tasks
+      }
+    }
   }
 
-  if (todolist.filter === "completed") {
-    tasksForTodolist = tasksForTodolist?.filter((task) => task.status === TaskStatus.Completed)
-  }
+  const tasksForTodolist = filterTasks(tasks, todolist.filter)
 
   return (
     <>
-      {tasksForTodolist?.length === 0 ? (
+      {tasksForTodolist.length === 0 ? (
         <p>Тасок нет</p>
       ) : (
         <List>
-          {tasksForTodolist?.map((task) => {
+          {tasksForTodolist.map((task) => {
             return <Task key={task.id} task={task} todolist={todolist} />
           })}
         </List>
